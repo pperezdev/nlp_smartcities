45ee3925c79922af3231eafed323086c7ed6cb83
pperezdev <pierrick.perez@free.fr>
2022-12-28 12:12:05 +0100

    update functions for flask

--- app/functions.py
+++ app/functions.py
@@ -4,5 +4,8 @@ def get_models_name() -> list(str):
     fm = FileManagers()
     return fm.get_models_name_list()
 
-def select_model(model_name:str):
-    pass
+def get_synthesis_url(model_name:str, url:str) -> str:
+    return ""
+
+def get_synthesis_text(model_name:str, text:str) -> str:
+    return ""
